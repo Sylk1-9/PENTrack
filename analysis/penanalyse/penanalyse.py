f702cf663508c9e5acbedeeba44ce4a5ba6cf2bf
svannest <svannest@uni-mainz.de>
2023-07-03 23:21:54 +0200

    animation plt for penanalyse

--- analysis/penanalyse/penanalyse.py
+++ analysis/penanalyse/penanalyse.py
@@ -1,5 +1,6 @@
 import os as os
 import glob as glob
+import scipy as scipy
 import h5py as h5py
 import numpy as np
 import pandas as pd
@@ -14,7 +15,7 @@ import magpylib as magpy
 import magtspect as magt
 from colorsys import hls_to_rgb
 import numpy as np 
-
+import time
 
 def get_distinct_colors(n):
     colors = []
@@ -39,18 +40,18 @@ magpy.defaults.display.style.current.arrow.width = 4
 
 
 # stl_file_paths = glob.glob(workingdir + "in/CAD/*.stl") + glob.glob(workingdir + "in/CAD/store/*.stl")
-# class SetVisibilityCallback:
-#     """Helper callback to keep a reference to the actor being modified."""
+class SetVisibilityCallback:
+    """Helper callback to keep a reference to the actor being modified."""
 
-#     def __init__(self, actors):
-#         self.actors = actors
+    def __init__(self, actors):
+        self.actors = actors
 
-#     def __call__(self, state):
-#         if isinstance(self.actors, (list, tuple, set, dict)):
-#             for actor in self.actors:
-#                 actor.SetVisibility(state)
-#         else:
-#             self.actors.SetVisibility(state)
+    def __call__(self, state):
+        if isinstance(self.actors, (list, tuple, set, dict)):
+            for actor in self.actors:
+                actor.SetVisibility(state)
+        else:
+            self.actors.SetVisibility(state)
 
 
 
@@ -76,8 +77,7 @@ class MyCustomRoutine:
 
             
 class data:    
-
-    def __init__(self, dfile="000000000001", dfilext=".h5", keys=('nt'), loadstl=True, ptdir="/home/sly/Work/Physics/Neutrons/tSPECT/PENtrack/pentrack/"):
+    def __init__(self, dfile="000000000002", dfilext=".h5", keys=('nt'), loadstl=True, plotter=None, ptdir="/home/sly/Work/Physics/Neutrons/tSPECT/PENtrack/pentrack/"):
         self.ptdir = ptdir
         self.keys = keys
         self.dfile = dfile
@@ -85,20 +85,18 @@ class data:
         self.df = {}
         self.stlfiles = []
         self.stlactors = []
-        # self.fig = fig
-        # self.plotter = plotter
         self.loadata()
         if loadstl:
             self.loadstl()
-        self.axes = None
-        self.plotter = None
+        # self.axes = None
+        self.plotter = plotter
             
     def loadata(self):
         if (("h5" in self.dfilext) or ("hdf5" in self.dfilext))  :
             fh5 = h5py.File(self.ptdir + "out/" + self.dfile + ".h5",'r')
             for key in fh5.keys():
                 if (key not in 'config') and (key in keydic):
-                    print("loading " + key + " in dataframe df")
+                    # print("loading " + key + " in dataframe df")
                     
                     self.df[keydic[key]] = pd.DataFrame(fh5[key][:])
                 else:
@@ -111,7 +109,8 @@ class data:
                     outpathc = outpath
                     print("loading " + key + " in dataframe df")
                     self.df[keydic[key]] = pd.read_csv(outpathc, sep=" ")
-        
+
+            # # Todo :  iterator for large file?
             # spinlist = []
             # data_iter = pd.read_hdf(workingdir + "out/saved/" + config_name + ".h5", key='neutronspin', columns=['x', 'y', 'z', 'Sx', 'Sy', 'Sz'], iterator=True, chunksize=100, where=None, start=50000000)
             # it = 0
@@ -149,25 +148,18 @@ class data:
 
 
     def initplotter(self):
-        # axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
         plotter = pvqt.BackgroundPlotter()
         plotter.show_bounds(color="black")
         return plotter
-        
-    # def toggle_stl(flag):
-    #     for actor in self.stlactors:
-    #         actor.SetVisibility(flag)
-        
-    # def toggle_actor(self, flag):
-    #     for actor in self.stlactors:
-    #         actor.SetVisibility(flag)
+
+    
     def opacity_stl(self, opacity):
         for actor in self.stlactors:
             actor.GetProperty().SetOpacity(opacity)
-                
+
+            
     def plotstl(self, plotter=None, opacity=1, stlfiles=None):
 
-        # self.axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5) if self.axes is None else self.axes
         self.plotter = self.initplotter() if self.plotter is None else self.plotter
         self.stlactors = []
         stlfiles = self.stlfiles if stlfiles is None else stlfiles
@@ -178,16 +170,12 @@ class data:
 
         
         for stlfile, color in zip(stlfiles, colors):
-            print(stlfile)
-            # color= tuple(np.random.randint(0, 255, 3)) + (1,)
             mesh = pv.read(stlfile).scale([1000.0, 1000.0, 1000.0], inplace=False)
-            # engine = MyCustomRoutine(mesh)
             # mesh = mesh.rotate_y(-90, point=axes.origin, inplace=False)
             actor = self.plotter.add_mesh(mesh, opacity=opacity, color=color)
             
             self.stlactors.append(actor)
             
-            # Make a separate callback for each widget
             callback = SetVisibilityCallback(actor)
             self.plotter.add_checkbox_button_widget(
                 callback,
@@ -201,15 +189,10 @@ class data:
             )
             pos = pos + size + (size // 10)
         
-        # self.plotter.add_checkbox_button_widget(self.toggle_actor, size=30, position=(10, 10), color_on="blue", value=True)
-        self.plotter.add_slider_widget(self.opacity_stl, (0.0, 1.0), pointa=(0.04, 0.6), pointb=(0.04, 0.9),  slider_width=0.02, tube_width=0.005, color="black")
+        self.plotter.add_slider_widget(self.opacity_stl, (0.0, 1.0), value=opacity, pointa=(0.04, 0.6), pointb=(0.04, 0.9),  slider_width=0.02, tube_width=0.005, color="black")
 
-                
         return self.plotter, self.stlactors
 
-    # def toggle_mag(flag):
-    #     for actor in self.magactors:
-    #         self.magactors[actor].SetVisibility(flag)
 
     def opacity_mag(self, opacity):
         for actor in self.magactors:
@@ -217,10 +200,7 @@ class data:
 
             
     def plotmag(self, opacity=1):
-
-        # magpy.defaults.display.style.base.opacity = opacity
         
-        # self.axes = if self.axes is None else self.axes
         self.plotter = self.initplotter() if self.plotter is None else self.plotter
         self.stlactors = []
       
@@ -230,17 +210,12 @@ class data:
         sf1 = magt.spinflipper(isf=1, kseg=20).source#.rotate_from_angax(-90, axis='y', anchor=0)
         sf2 = magt.spinflipper(isf=2, kseg=20).source#.rotate_from_angax(-90, axis='y', anchor=0)
 
-        # coll.set_children_styles(magnetization_color_south="blue")
-        
         size = 30
         pos = 2*size
         sources = [co, octu, comp, sf1, sf2]
         prevactors = self.plotter.actors.copy()
         colors = get_distinct_colors(len(sources))
         for source, color in zip(sources, colors):
-            # color = 'rgb(%i, %i, %i)' % tuple(np.random.randint(0, 255, 3))
-            # color= tuple(np.random.randint(0, 255, 3),) # + (1,)
-            # source.set_children_styles(color='rgb(%i, %i, %i)' % color)
             source.set_children_styles(color=color)
             source.set_children_styles(opacity=opacity)
              
@@ -248,7 +223,6 @@ class data:
             self.plotter = source.show(backend='pyvista', canvas=self.plotter, return_fig=True, style_magnetization_show=False)
             actor = [v for k,v in self.plotter.actors.items() if k not in oldvactors]
 
-            # Make a separate callback for each widget
             callback = SetVisibilityCallback(actor)
             self.plotter.add_checkbox_button_widget(
                 callback,
@@ -263,98 +237,66 @@ class data:
             pos = pos + size + (size // 10)
 
         self.magactors = [v for k,v in self.plotter.actors.items() if k not in prevactors]
-        # self.plotter.add_checkbox_button_widget(self.toggle_actor, size=30, position=(40, 10), color_on="green", value=True)
-
-        self.plotter.add_slider_widget(self.opacity_mag, (0, 1), pointa=(0.6, 0.03), pointb=(0.9, 0.03), slider_width=0.02, tube_width=0.005, color="black")
+        self.plotter.add_slider_widget(self.opacity_mag, (0, 1), value=opacity, pointa=(0.6, 0.03), pointb=(0.9, 0.03), slider_width=0.02, tube_width=0.005, color="black")
 
         return self.plotter, self.magactors
 
 
+    def plotends(self, state="start", ptype="neutron", color=None):
+        if (ptype == 'n' or ptype == "neutron"):
+            key = 'ne'
+        elif (ptype == 'p' or ptype == "proton"):
+            key = 'pe'
+        elif (ptype == 'p' or ptype == "electron"):
+            key = 'ee'
+
+        if color is None:
+            color = "black" if state == "start" else "red"
+        
+        self.plotter.add_mesh(1000 * da.df[key][['x'+state, 'y'+state, 'z'+state]].values, color=color, render_points_as_spheres=True, point_size=10)
+
+        return self.plotter
+        
 
 da = data()
-pl, magactors = da.plotmag(opacity=0.9)
-pl, stlactors = da.plotstl(opacity=0.3)
-
-# for actor in stlactors:
-#     actor.GetProperty().SetOpacity(0.1)
-
-####################
-
-# magpy.defaults.display.style.magnet.magnetization.color.mode = 'bicolor'
-# magpy.defaults.display.style.base.path.line.width = 10
-# magpy.defaults.display.style.markers.path.line.width
-
-# axes = pv.Axes(show_actor=True, actor_scale=1.0, line_width=2)
-
-#######################
-
-# pl = pv.Plotter()
-
-# co = magt.coil(wx=200, wr=50).source
-# octu = magt.octupole(nzseg=1, stype="cyl").source
-# comp = magt.octupole(nzseg=1, stype="cyl", comp=True).source
-# sf1 = magt.spinflipper(isf=1, kseg=20).source.rotate_from_angax(-90, axis='y', anchor=0)
-# sf2 = magt.spinflipper(isf=2, kseg=20).source.rotate_from_angax(-90, axis='y', anchor=0)
-# coll = magpy.Collection([co, octu, comp, sf1, sf2])
-# # coll = magpy.Collection([sf1, sf2])
-
-# pl = coll.show(backend='pyvista', plotter=pl, return_fig=True, style_magnetization_show=False)
-# da = data()
-# da.plotstl(plotter=pl, axes=axes, opacity=0.2)
-# pl.show_bounds(color="black")
-
-# # pl.show(interactive_update=True, interactive=True, auto_close=False)
-# pl.show(interactive_update=False, interactive=True, auto_close=False)
-# # pl.show(interactive_update=True)
+# pl, magactors = da.plotmag(opacity=0)
+pl, stlactors = da.plotstl(opacity=0.01)
+# pl = da.plotends("end")
 
 
 
-#######################
+df_nts = da.df['nt'].sort_values(['particle', 't'])
 
-# pl = BackgroundPlotter()
+points = pv.PolyData(1000 * da.df['ne'].sort_values('particle')[['xstart', 'ystart', 'zstart']].values)
+points['particle'] = da.df['ne'].sort_values('particle')['particle']
+points['t'] = da.df['ne']['tstart']
 
-# pl = pv.Plotter()
-# pl = BackgroundPlotter()
+cloud = pv.PolyData(points)
+pl.add_points(cloud, color='blue', render_points_as_spheres=True, point_size=10)
 
-# co = magt.coil(wx=20, wr=50).source.rotate_from_angax(90, axis='y', anchor=0)
-# octu = magt.octupole(nzseg=1, stype="cyl").source.rotate_from_angax(90, axis='y', anchor=0)
-# comp = magt.octupole(nzseg=1, stype="cyl", comp=True).source.rotate_from_angax(90, axis='y', anchor=0)
-# sf1 = magt.spinflipper(isf=1, kseg=20).source#.rotate_from_angax(-90, axis='y', anchor=0)
-# sf2 = magt.spinflipper(isf=2, kseg=20).source#.rotate_from_angax(-90, axis='y', anchor=0)
+particles = df_nts['particle'].unique()
 
-# # coll = magpy.Collection([co, octu, comp, sf1, sf2])
-# # coll = magpy.Collection([sf1, sf2])
 
-# pl = co.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# pl = octu.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# pl = comp.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# pl = sf1.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# pl = sf2.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# # pl = coll.show(backend='pyvista', canvas=pl, return_fig=True, style_magnetization_show=False)
-# magactors = pl.actors.copy()
-
-# da = data()
-# pl, stlactors = da.plotstl(plotter=pl, axes=axes, opacity=0.5)
+interpolation_functions = {}
+for particle, data in df_nts.groupby('particle'):
+    positions = data[['x', 'y', 'z']].values
+    times = data['t'].values
     
-# def toggle_stl(flag):
-#     for actor in stlactors:
-#         actor.SetVisibility(flag)
+    interpolation_functions[particle] = scipy.interpolate.interp1d(times, positions, axis=0, bounds_error=False, fill_value=(data[['x', 'y', 'z']].iloc[0], data[['x', 'y', 'z']].iloc[-1]))
 
-# def toggle_mag(flag):
-#     for actor in magactors:
-#         magactors[actor].SetVisibility(flag)
+    
+time_stamp = 0.0
+dt = 0.01
+def update_animation():
+    global time_stamp
+    time_stamp += dt
+    interpolated_positions = []
+    for particle, interpolation_func in interpolation_functions.items():
+        interpolated_positions.append(interpolation_func(time_stamp))
 
-
-# pl.add_checkbox_button_widget(toggle_stl, position=(10, 10), value=True)
-# pl.add_checkbox_button_widget(toggle_mag, position=(100, 10), value=True)
-# pl.show_bounds(color="black")
-
-# # pl.enable_terrain_style()
-# pl.show()
-
-# pl.update()
-# # pl.show(interactive_update=True, interactive=True, auto_close=False)
-# # pl.show(interactive_update=False, interactive=True, auto_close=False)
-# # pl.show(interactive_update=True)
+    points.points = 1000 * np.array(interpolated_positions)
+    pl.update()
+    
+pl.add_callback(update_animation, 10, int(5/dt))
 
 
