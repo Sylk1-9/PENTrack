faf5541c109c60f965482a50d0a6cfddf701401e
svannest <svannest@uni-mainz.de>
2023-07-07 03:22:26 +0200

    rgba for Total and Kinetic energy.

--- analysis/penanalyse/penanalyse.py
+++ analysis/penanalyse/penanalyse.py
@@ -134,6 +134,16 @@ class data:
             else:
                 self.actors.SetVisibility(state)
 
+
+    # class SetRGBACallback:
+    #     """Helper callback to keep a reference to the actor being modified."""
+
+    #     def __init__(self, plotter):
+    #         self.plotter = plotter
+
+    #     def __call__(self, state):
+    #         if self.plotter.rgba == 'H'
+
                 
     class ScreenshotCallback:
         """Helper callback to keep a reference to the actor being modified."""
@@ -173,9 +183,11 @@ class data:
                     pygifsicle.optimize(self.gifname) # For overwriting the original one
 
     
+    
     def initplotter(self):
         plotter = pvqt.BackgroundPlotter(window_size=(2*1024, 2*768), multi_samples=2)
         plotter.show_bounds(color="black")
+        plotter.set_background('aliceblue', top='white')
         return plotter
 
     
@@ -218,7 +230,7 @@ class data:
 
         return self.plotter
 
-
+    
     def opacity_mag(self, opacity):
         for actor in self.plotter.magactors:
             actor.GetProperty().SetOpacity(opacity)
@@ -332,9 +344,15 @@ class data:
     def speedanime(self, speed):
         self.plotter.dt = self.plotter.idt * np.sign(speed) * speed**2
 
+        
+    def linlin(self, x, xmin, xmax):
+        conv = (((x - xmin) * (1 - 0)) / (xmax - xmin)) + 0
+        conv = conv * (1 - (conv < 0))
+        conv = conv * (1 - (conv > 1)) + (conv > 1)
+        return conv
 
     
-    def animate(self, ti=0, tf=None, dt=0.005, fps=10, spath="/home/sly/Work/Physics/Neutrons/tSPECT/PENtrack/plots/animations/penanalyse/", pselect=None, minp=0, maxp=None, displaypID=False):
+    def animate(self, ti=0, tf=None, dt=0.005, fps=10, spath="/home/sly/Work/Physics/Neutrons/tSPECT/PENtrack/plots/animations/penanalyse/", pselect=None, minp=0, maxp=None, minE=None, maxE=None, minH=None, maxH=None, displaypID=False):
         """
         Main function for animation
         """
@@ -357,6 +375,13 @@ class data:
         df_nts = df_nts.sort_values(['particle', 't'])
         df_ne = df_ne.sort_values(['particle'])
 
+        minE = df_nts.min()['E'] if minE is None else minE
+        maxE = df_nts.max()['E'] if maxE is None else maxE
+
+        minH = df_nts.min()['H'] if minH is None else minH
+        maxH = df_nts.max()['H'] if maxH is None else maxH
+        
+        
         if tf is not None:
             df_nts = df_nts[df_nts['t'] < tf]
         else:
@@ -366,30 +391,56 @@ class data:
         cloud = pv.PolyData(1000 * df_ne[['xstart', 'ystart', 'zstart']].values)
 
         rgba = np.array(pv.Color(pcolor).int_rgba)[None] * np.ones((cloud.n_faces, 4))
-        rgba[:, 0] = 255//2 + 75*df_ne['polstart'].values
-        rgba[:, 1] = 255//2 - 75*df_ne['polstart'].values
-        rgba[:, 2] = 120
+        rgba[:, 1] = 255//2 + 75*df_ne['polstart'].values
+        rgba[:, 2] = 255//2 - 75*df_ne['polstart'].values
+        rgba[:, 0] = 120
         rgba[:, -1] = 1
         cloud["rgba"] = rgba
 
         self.plotter.pactor = self.plotter.add_points(cloud, render_points_as_spheres=True, point_size=8, scalars="rgba", rgba=True, lighting=lighting)
 
+        
+        callback = self.SetVisibilityCallback(self.plotter.pactor)
+        self.plotter.add_checkbox_button_widget(
+            callback,
+            value=True,
+            position=(100, 1),
+            size=30,
+            border_size=1,
+            color_on="aquamarine",
+            color_off='grey',
+            background_color='grey',
+        )
+
         if displaypID:
             cloud["pID"] = [f"{int(i)}" for i in df_ne['particle']]
 
-            self.plotter.labelpactor = self.plotter.add_point_labels(cloud, "pID", font_size=20, render_points_as_spheres=True, point_size=6)
+            self.plotter.labelpactor = self.plotter.add_point_labels(cloud, "pID", font_size=14, render_points_as_spheres=True, point_size=6)
 
-            callback = self.SetViqsibilityCallback(self.plotter.labelpactor)
+            callback = self.SetVisibilityCallback(self.plotter.labelpactor)
             self.plotter.add_checkbox_button_widget(
                 callback,
                 value=True,
-                position=(550, 40),
+                position=(100, 40),
                 size=30,
                 border_size=1,
-                color_on="black",
+                color_on="burlywood",
                 color_off='grey',
                 background_color='grey',
             )
+
+        self.plotter.rgba = 'polarisation'
+        # callback = self.RGBACallback(self.plotter)
+        # self.plotter.add_checkbox_button_widget(
+        #     callback,
+        #     value=False,
+        #     position=(100, 80),
+        #     size=30,
+        #     border_size=1,
+        #     color_on="gray",
+        #     color_off='chocolate',
+        #     background_color='grey',
+        # )
 
 
         callback = self.RecordGifCallback(self.plotter, self.dfile, spath, fps=5)
@@ -404,7 +455,6 @@ class data:
             background_color='grey',
         )
         
-        
         callback = self.ScreenshotCallback(self.plotter, self.dfile, spath)
         self.plotter.add_checkbox_button_widget(
             callback,
@@ -416,22 +466,37 @@ class data:
             color_off='lime',
             background_color='grey',
         )
-        
 
+        callback = self.ScreenshotCallback(self.plotter, self.dfile, spath)
+        self.plotter.add_checkbox_button_widget(
+            callback,
+            value=False,
+            position=(800, 40),
+            size=30,
+            border_size=1,
+            color_on="gray",
+            color_off='lime',
+            background_color='grey',
+        )
+
+        
+        
         interpolation_functions = {}
         for particle, data in df_nts.groupby('particle'):
-            positions = data[['x', 'y', 'z', 'polarisation']].values
+            positions = data[['x', 'y', 'z', 'polarisation', 'E', 'H']].values
             times = data['t'].values
-            interpolation_functions[particle] = scipy.interpolate.interp1d(times, positions, axis=0, bounds_error=False, fill_value=(data[['x', 'y', 'z', 'polarisation']].iloc[0], data[['x', 'y', 'z', 'polarisation']].iloc[-1]))
+            interpolation_functions[particle] = scipy.interpolate.interp1d(times, positions, axis=0, bounds_error=False, fill_value=(data[['x', 'y', 'z', 'polarisation', 'E', 'H']].iloc[0], data[['x', 'y', 'z', 'polarisation', 'E', 'H']].iloc[-1]))
 
         self.plotter.time_stamp = ti
         self.plotter.idt = dt
-        self.plotter.timetxt = self.plotter.add_text("t=%g [s]"%self.plotter.time_stamp, position='lower_right', font_size=18)
+        self.plotter.titletxt = self.plotter.add_text(self.dfile, position='upper_right', font_size=18, color="black")
+        self.plotter.timetxt = self.plotter.add_text("t=%g [s]"%self.plotter.time_stamp, position='lower_right', font_size=18, color="black")
         
         def update_animation():
             if ((self.plotter.dt < 0) and (self.plotter.time_stamp > ti)) or ((self.plotter.dt > 0) and (self.plotter.time_stamp < tf)):
                 self.plotter.time_stamp += self.plotter.dt
-                self.plotter.timetxt.SetText(1, "t=%g [s]"%self.plotter.time_stamp)
+                # self.plotter.timetxt.SetText(1, "t=%.4g [s]"%self.plotter.time_stamp)
+                self.plotter.timetxt.SetText(1, "t={:8.3f} [s]".format(self.plotter.time_stamp))
                 # self.plotter.update_text("t=%g [s]"%self.plotter.time_stamp, self.plotter.timetxt)
                 
                 interpolated_vals = []
@@ -439,20 +504,34 @@ class data:
                     interpolated_vals.append(interpolation_func(self.plotter.time_stamp))
 
                 interpolated_vals = np.array(interpolated_vals)
-                cloud.points = 1000 * interpolated_vals[:, :3]
+                cloud.points = 1000 * interpolated_vals[:, 0:3]
 
-                # rgba = np.ones((len(interpolated_vals), 4))# np.array(pv.Color(pcolor).int_rgba)[None] * np.ones((cloud.n_faces, 4))
-                cloud["rgba"][:, 0] = 255//2 + 75*interpolated_vals[:, 3]
-                cloud["rgba"][:, 1] = 255//2 - 75*interpolated_vals[:, 3]
-                cloud["rgba"][:, 3] = 1.0 * np.array(self.plotter.time_stamp < df_ne['tend']).astype(int)
+                rgba = 120 * np.ones((len(interpolated_vals), 4))
+                # np.array(pv.Color(pcolor).int_rgba)[None] * np.ones((cloud.n_faces, 4))
+                if self.plotter.rgba == 'polarisation':
+                    rgba[:, 0] = 255//2 + 100*interpolated_vals[:, 3]
+                    rgba[:, 2] = 255//2 - 100*interpolated_vals[:, 3]
+                    
+                #     cloud["rgba"][:, 0] = 255//2 + 75*interpolated_vals[:, 3]
+                #     cloud["rgba"][:, 1] = 255//2 - 75*interpolated_vals[:, 3]
+                #     cloud["rgba"][:, 3] = 1.0 * np.array(self.plotter.time_stamp < df_ne['tend']).astype(int)
+                
+                elif self.plotter.rgba == 'E':
+                    rgba[:, 1:3] = 255 * self.linlin(interpolated_vals[:, 4], minE, maxE)[:, None]
+                    # print(min(rgba[:, 0]), max(rgba[:, 0]))
+                elif self.plotter.rgba == 'H':
+                    rgba[:, 1:3] = 255 * self.linlin(interpolated_vals[:, 5], minH, maxH)[:, None]
+
+                rgba[:, 3] = 1.0 * np.array(self.plotter.time_stamp < df_ne['tend']).astype(int)
+                cloud["rgba"] = rgba
                 
                 # self.plotter.render()
                 self.plotter.update()
-
+                    
                 if self.plotter.savegif:
                     self.plotter.write_frame()
             
-        self.plotter.add_slider_widget(self.speedanime, (-4, 4), value=1, pointa=(0.5, 0.03), pointb=(0.8, 0.03), slider_width=0.02, tube_width=0.005, color="white")
+        self.plotter.add_slider_widget(self.speedanime, (-4, 4), value=1, pointa=(0.5, 0.03), pointb=(0.8, 0.03), slider_width=0.02, tube_width=0.005, color="aquamarine")
 
         self.plotter.add_callback(update_animation, int(1000/fps), None)
         return self.plotter
@@ -461,7 +540,7 @@ class data:
 
 # name of datafile
 # dfile = "000000000105"
-dfile = "000000000010"
+dfile = "000000000020"
 
 # instantiate data object
 da = data(dfile)
@@ -482,5 +561,5 @@ pl = da.plotlogs(ptype="n", state="end", pselect=pselect, color="deepskyblue")
 pl = da.plotlogs(ptype="n", state="hit", pselect=pselect, color="darkorchid")
 
 # # play animation
-pl = da.animate(dt=0.01, fps=10, pselect=pselect, minp=0, maxp=None)
+pl = da.animate(dt=0.01, fps=10, pselect=pselect, minp=0, maxp=None, minE=0, maxE=5e-8, minH=0, maxH=1e-6, displaypID=False)
 
